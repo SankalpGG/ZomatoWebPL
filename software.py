d62c2097af8b9237369af6d48f825a46d7d3107f
SankalpGG <sankalpvish.1@gmail.com>
2024-07-30 15:39:02 +0530

    software.py

--- software.py
+++ software.py
@@ -1,1 +1,12 @@
 #ZomatoXBlinkit
+import getpass
+id={'user':123456}
+
+def login():
+    k=input("Enter your username: ")
+    v=getpass.getpass("Enter Your Pass: ")
+    if id[k]==v:
+        print("Login Successful")
+    else:
+        print("Invalid Entry")
+
