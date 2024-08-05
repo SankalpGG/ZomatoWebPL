15550429ddf423e0fd825904ba3dea85fe8dfce3
SankalpGG <sankalpvish.1@gmail.com>
2024-08-05 16:14:09 +0530

    Update software.py

--- software.py
+++ software.py
@@ -1,10 +1,9 @@
 #ZomatoXBlinkit
-import getpass
-id={'user':123456}
+id={'user':"123456"}
 
 def login():
     k=input("Enter your username: ")
-    v=getpass.getpass("Enter Your Pass: ")
+    v=input("Enter Your Pass: ")
     if id[k]==v:
         print("Login Successful")
     else:
