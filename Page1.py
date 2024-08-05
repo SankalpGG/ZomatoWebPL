a3b05145d4d5b5e06b68706e68f70ffa79da0e11
SankalpGG <sankalpvish.1@gmail.com>
2024-08-05 11:48:10 +0000

    update page1.py

--- Page1.py
+++ Page1.py
@@ -13,6 +13,7 @@ def new_user():
     v=input("Enter your password: ")
     id[k]=v
     print("New Id Successfully Created")
+
 print("""
 ###----WELCOME--TO--ZOMATO----###
 Enter:
@@ -20,3 +21,9 @@ Enter:
 2 for creating a new account
 
 """)
+
+n=int(input(">>>"))
+if n==1:
+    login()
+if n==2:
+    new_user()
