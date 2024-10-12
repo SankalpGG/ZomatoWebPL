5a14dffae9c2498029945e75820a8a2c4843c21b
SankalpGG <sankalpvish.1@gmail.com>
2024-10-12 12:08:45 +0530

    Add files via upload

--- software.py
+++ software.py
@@ -1,12 +1,70 @@
 #ZomatoXBlinkit
-id={'user':"123456"}
+
+id={'user':123456}
 
 def login():
     k=input("Enter your username: ")
     v=input("Enter Your Pass: ")
-    if id[k]==v:
+    if k in id and id[k]==v:
         print("Login Successful")
     else:
-        print("Invalid Entry")
+        print("Invalid Credentials")
 
 login()
+
+def register():
+    k=input("enter user name ")
+    if k in id:
+        print("username already exists")
+        register()
+    else:
+        v=input("enter your password ")
+        v1=input("enter your password again ")
+        if v==v1:
+            id[k]=v
+            print("registered succesfully")
+        else:
+            print("password mismatch")
+            register()
+
+register()
+
+def home():
+    print("Welcome to Home Page")
+    print('''
+1. To order Food
+2.  To DineIn
+3.  To View Discount Code Available
+4.  To View Cart
+5.  To Edit Dilevery Address
+6.  To View Popular Today
+7.  To View Order History
+8.  To Edit Payment Options
+9.  To Delete Your Account
+10.  To Logout
+''')
+    n=int(input('>'))
+    if n==1:
+        print("Order Food")
+    if n==2:
+        print("DineIn")
+    if n==3:
+        print('Viewing Discount Codes')
+    if n==4:
+        print('Your Cart')
+    if n==5:
+        print('Edit Delivery Address')
+    if n==6:
+        print('Popular Today')
+    if n==7:
+        print('Order History')
+    if n==8:
+        print('Edit Payment Options')
+    if n==9:
+        print('To Delete Your Account')
+    if n==10:
+        print('You have been logged out')
+
+home()
+
+     
